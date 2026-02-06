184104d244fcfe4e73bf1f95e0e4008b687b9598
Virus861 <mohasinmulla861@gmail.com>
2026-02-06 11:55:33 +0530

    updated setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup 
+
+setup(
+    name='Medibot',
+    version='0.1.0',
+    author='Mohasin Mulla',
+    description='A medical chatbot application',
+    packages=find_packages(),
+    install_requires=[]
+
+    )
